6f0201a1e79c2b7d6e637c42d9f0bb198e16e6cf
madjiebimaa <madjiebimaa@gmail.com>
2022-04-11 16:50:52 +0700

    chore: init MikroORM setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import { MikroORM } from "@mikro-orm/core";
+import mikroOrmConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+};
+
+main().catch((err) => {
+  console.log(err);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+export default {
+  type: "postgresql",
+  dbName: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  entities: [Post],
+  debug: !__prod__,
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    glob: "!(*.d).{js,ts}",
+  },
+  allowGlobalContext: true,
+} as Parameters<typeof MikroORM.init>[0];
